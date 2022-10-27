04128c3b3d40924ae409901eff7557d3bb43c796
Parv Jetalkumar Dave <pdave6@asurite.asu.edu>
2022-10-27 15:41:38 -0700

    Added Skeleton

---
+++ src/Urinals.java
@@ -0,0 +1,24 @@
+
+
+public class Urinals
+{
+    static int countUrinals( String str ){
+        System.out.println("countUrinals");
+        return 0;
+    }
+    static Boolean goodString( String str ) {
+        System.out.println("Good String Check Function");
+
+        return true;
+    }
+    static String openFile(){
+        String str = null;
+        return str;
+    }
+
+    public static void main(String[] args) {
+        String st = "100001";
+        goodString(st);
+// 		System.out.println("Hello World");
+    }
+}
---
+++ src/UrinalsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void countUrinals() {
+        System.out.println("countUrinal is called");
+    }
+
+    @org.junit.jupiter.api.Test
+    void goodString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void openFile() {
+    }
+}
