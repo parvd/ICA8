8ab49e11682fca5cf52b3190975c968248432e7b
Parv Jetalkumar Dave <pdave6@asurite.asu.edu>
2022-10-27 18:39:09 -0700

    Added Testcase for goodstring and countUrinals

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,24 +1,86 @@
 
-
+import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class Urinals
 {
-    static int countUrinals( String str ){
+     int countUrinals( String str ){
         System.out.println("countUrinals");
-        return 0;
+         int cnt=0;
+         int n = str.length();
+//         System.out.println(n);
+         if(str.charAt(0)=='0' && str.charAt(1)=='0'){
+             cnt++;
+             StringBuffer string = new StringBuffer(str);
+             string.setCharAt(0, '1');
+             str = String.valueOf(string);
+//             System.out.println(str);
+             // str.charAt(0)=1;
+         }
+         {
+             for(int i=1;i<n-1;i++){
+//                 System.out.println(str);
+//                 System.out.println(str.charAt(i-1) + str.charAt(i) + str.charAt(i+1));
+                 if(str.charAt(i-1)=='0' && str.charAt(i+1)=='0'  && str.charAt(i)=='0'){
+//                    System.out.println("i="+i);
+//                    System.out.println("here");
+                     cnt++;
+                     StringBuffer string = new StringBuffer(str);
+                     string.setCharAt(i, '1');
+                     str = String.valueOf(string);
+//                     System.out.println(str);
+                 }
+             }
+             if(str.charAt(n-1)=='0' && str.charAt(n-2)=='0'){
+                 cnt++;
+                 StringBuffer string = new StringBuffer(str);
+                 string.setCharAt(n-1, '1');
+                 str = String.valueOf(string);
+//                System.out.println(str);
+             }
+         }
+         return cnt;
     }
-    static Boolean goodString( String str ) {
-        System.out.println("Good String Check Function");
+     int goodString( String str ) {
+         System.out.println("Good String Check Function");
+         int flag=0;
+         int n = str.length();
+         if(n>21)
+             return -1;
+         if(str.charAt(0) == '1' && str.charAt(1)=='1'){
+             return -1;
+         }
+         for(int i=1;i<n-2;i++){
+             if(str.charAt(i)=='0' || str.charAt(i)=='1' ){
+                    if((str.charAt(i-1)==1 || str.charAt(i+1)==1) && str.charAt(i)==1 ){
+                        return -1;
+                    }
+             }
+             else{
+                 continue;
+             }
+         }
+         if(str.charAt(n-2) == '1' && str.charAt(n-1)=='1'){
+             return -1;
+         }
+         return 1;
+    }
+     String openFile() throws IOException {
+         Path fileName = Path.of("C:\\Users\\pdave6\\IdeaProjects\\ICA8\\urinal.dat");
 
-        return true;
-    }
-    static String openFile(){
-        String str = null;
-        return str;
+         // Now calling Files.readString() method to
+         // read the file
+         String str = Files.readString(fileName);
+
+         // Printing the string
+         System.out.println(str);
+         return str;
     }
 
     public static void main(String[] args) {
         String st = "100001";
-        goodString(st);
+//        goodString(st);
 // 		System.out.println("Hello World");
     }
 }
--- src/UrinalsTest.java
+++ src/UrinalsTest.java
@@ -1,14 +1,32 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrinalsTest {
 
-    @org.junit.jupiter.api.Test
-    void countUrinals() {
+    @Test
+    void getCountUrinals() {
         System.out.println("countUrinal is called");
+        Urinals url = new Urinals();
+        assertTrue((url.countUrinals("10001")) == 1);
+        assertTrue((url.countUrinals("10101")) == 0);
+        assertTrue((url.countUrinals("00000")) == 3);
+        assertTrue((url.countUrinals("10000")) == 2);
+        assertTrue((url.countUrinals("010")) == 0);
+        assertTrue((url.countUrinals("100010")) == 1);
+
     }
 
     @org.junit.jupiter.api.Test
     void goodString() {
+        System.out.println("goodString is called");
+        Urinals url = new Urinals();
+        assertTrue((url.goodString("10111")) == -1);
+        assertTrue((url.goodString("10101")) == 1);
+        assertTrue((url.goodString("110000")) == -1);
+        assertTrue((url.goodString("100001")) == 1);
+        assertTrue((url.goodString("011")) == -1);
+        assertTrue((url.goodString("100010101010101010101010101010100000001")) == -1);
     }
 
     @org.junit.jupiter.api.Test
